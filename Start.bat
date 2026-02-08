5356709338c0a4ba4ca905ccedccf9faf1bebf61
Luke <luketanti@gmail.com>
2026-02-08 21:33:09 +0100

    Move Start.bat to repository root

--- Start.bat
+++ Start.bat
@@ -4,4 +4,4 @@ REM set "OWNFOIL_TRUST_PROXY_HEADERS=1"
 REM set "OWNFOIL_TRUSTED_PROXIES=127.0.0.1,192.168.1.0/24"
 REM The below key is for development use only!! Do not use it for deployment
 set "OWNFOIL_SECRET_KEY=MyyR9E6O9mAeJMUTtsBgLxbuY9OZdT742psExUsnPnT72veQ7rnPkAdhiDNihNR_KPvCj5K85DgL0Rmo4hUiSQ"
-python.exe .\app.py
+python.exe .\app\app.py
